99e0fe0686016dbc4006f6cf1c86b80948d39d8e
Danya <danyafrisina@hotmail.it>
2020-02-25 16:56:28 +0100

    commit solution

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function Counter() {
   const [counter, setCounter] = useState(0);
 
   return (
     <div>
+      <input
+        value={counter}
+        type="number"
+        onChange={e => setCounter(parseInt(e.target.value))}
+      />
       <p>The counter is at : {counter}</p>
       <button onClick={() => setCounter(counter + 1)}>> +1</button>
       <button onClick={() => setCounter(counter - 1)}>> -1</button>
